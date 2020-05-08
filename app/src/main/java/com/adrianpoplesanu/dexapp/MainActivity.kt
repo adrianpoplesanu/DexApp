dbd3f545426a15078b2881314b271d2a350417ef
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2020-05-08 08:47:35 +0300

    added event handler for signin button

--- app/src/main/java/com/adrianpoplesanu/dexapp/MainActivity.kt
+++ app/src/main/java/com/adrianpoplesanu/dexapp/MainActivity.kt
@@ -2,11 +2,23 @@ package com.adrianpoplesanu.dexapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val signinButton = findViewById<Button>(R.id.signinButton)
+        signinButton.setOnClickListener() {
+            signinButtonClickEvent(it)
+        }
+    }
+
+    private fun signinButtonClickEvent(view : View) {
+        Log.d("DexApp", "signin button clicked!")
     }
 }
